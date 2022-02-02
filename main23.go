4c7698e59fb36fccbee669155d9787108de2fd34
Kuschenko Egor <kushchenkoYA@gmail.com>
2022-02-02 16:04:58 +0300

    add task #23

---
+++ main23.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	sl := []int{22,12,45,1,77}
+	var n int
+	fmt.Println("Введите номер элемента, который хотите удалить:")
+	fmt.Scan(&n)
+	//Операции именно для номера элемента, а не для индекса
+	sl = append(sl[:n-1],sl[n:]...)
+	fmt.Println("Обновленный массив:",sl)
+
+}
